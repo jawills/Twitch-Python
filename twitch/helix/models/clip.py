6095e1e5872156d5e2ca2e4091e2fbbc9ff83c61
Justin Wills <justin.wills@g2technologies.com>
2021-03-24 22:21:03 -0700

    built out clip model

--- twitch/helix/models/clip.py
+++ twitch/helix/models/clip.py
@@ -8,3 +8,23 @@ class Clip(Model):
 
     def __init__(self, api: API, data: Dict[str, Any]):
         super().__init__(api, data)
+
+        print(data)
+        self.broadcaster_id: str = data.get('broadcaster_id')
+        self.broadcaster_name: str = data.get('broadcaster_name')
+        self.created_at: str = data.get('created_at')
+        self.creator_id: str = data.get('creator_id')
+        self.creator_name: str = data.get('creator_name')
+        self.embed_url: str = data.get('embed_url')
+        self.game_id: str = data.get('game_id')
+        self.id: str = data.get('id')
+        self.language: str = data.get('language')
+        self.pagination: str = data.get('pagination')
+        self.thumbnail_url: str = data.get('thumbnail_url')
+        self.title: str = data.get('title')
+        self.url: str = data.get('url')
+        self.video_id: str = data.get('video_id')
+        self.view_count: str = data.get('view_count')
+
+        def __str__(self):
+            return self.broadcaster_name
